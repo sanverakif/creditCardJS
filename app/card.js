a2f3dd8a2e3c3e932ff7f786ec0424798cfe958f
AKIF <sanveremreakif@gmail.com>
2022-01-31 14:37:44 +0300

    newData

--- app/card.js
+++ app/card.js
@@ -15,6 +15,7 @@ function validCreditCard(creditCardNumber) {
         return false
     }
 
+    debugger
     if (diffiranceNumberControl(creditCardNumber) == false) {
         return false
     }
@@ -33,8 +34,10 @@ function validCreditCard(creditCardNumber) {
 //tur kontrolu
 function checkNumber(creditCardNumber) {
     if (isNaN(creditCardNumber) == false) {
+        //alert("Sayıdır");
         return true
     }
+    //alert("Sayı değildir")
     return false
 }
 
@@ -42,36 +45,43 @@ function checkNumber(creditCardNumber) {
 function hasLength(creditCardNumber) {
     var _length = 16;
     if (_length == creditCardNumber.length) {
+        //alert(`Tüm değerlerin toplamı ${sum}' olmuştur ve 16'dan büyüktür`)
         return true
     }
+    //alert(`Tüm değerlerin toplamı ${sum}' olmuştur ve 16'dan küçüktür`)
     return false
 }
 
 //tek veya çift kontrolu
 function lastNumberMod2Control(creditCardNumber) {
     if (creditCardNumber[creditCardNumber.length - 1] % 2 == 0) {
-    //if (creditCardNumber[15] % 2 == 0) {
+        //alert(`${creditCardNumber[15]} değerinin modu 0'dır ve çifttir`)
         return true;
     }
+    //alert(`${creditCardNumber[15]} değerinin modu 1'dır ve tektir`)
     return false
 }
 
 //karakter toplamı ve kontrolu
 function sumNumber(creditCardNumber) {
     var sum = 0
+    //let numArr = [];
     for (let i = 0; i < creditCardNumber.length; i++) {
         sum += parseInt(creditCardNumber[i]);
+        //  sum += numArr[i]  
     }
 
     if (sum > 16) {
+        //alert("16 dan büyüktür")
         return true
     }
+    //alert("16 dan büyük olması gerekmektedir")
     return false
 }
 
 //En Az 2 sayı olmalı
 function diffiranceNumberControl(creditCardNumber) {
-
+    debugger
     var firstNumber = null;
     for (let i = 0; i < creditCardNumber.length; i++) {
 
